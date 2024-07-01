d99714f8442151d9b56c03666de7dad0e0e5e2a5
Mkhotamirais <mkhotamirais@gmail.com>
2024-07-02 06:53:45 +0700

    akhir

--- client/src/page/Apps/dash/Dash.jsx
+++ client/src/page/Apps/dash/Dash.jsx
@@ -6,12 +6,12 @@ export default function Dash() {
     <div>
       Dashboard
       <div>welcome {userData?.username}</div>
-      <p>
+      {/* <p>
         masalah cookie(sameSite: none/lax): this attempt to set a cookie via a Set-Cookie header was blocked becaues it
         had the SameSite=Lax attribut but came from a cross-site response which was not the response to a top-level
         navigation.
       </p>
-      <p>jika sameSite=none maka akan muncuk peringatan:</p>
+      <p>jika sameSite=none maka akan muncuk peringatan:</p> */}
     </div>
   );
 }
